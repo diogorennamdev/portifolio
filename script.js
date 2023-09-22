e7be617017561b4d6f7b0127e715d741efe73ef3
diogorennam <diogorenan006@gmail.com>
2023-09-21 22:41:12 -0300

    Adiconando responsividade

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ const nav = document.querySelector('.menu');
 
 hamburguer.addEventListener('click', () => (
     nav.classList.toggle('active')
-))
+));
+
+document.getElementById("leiaMaisBtn").addEventListener("click", function() {
+    var maisTexto = document.getElementById("maisTexto");
+    var btn = document.getElementById("leiaMaisBtn");
+
+    if (maisTexto.style.display === "none") {
+      maisTexto.style.display = "inline";
+      btn.innerHTML = "Leia menos";
+    } else {
+      maisTexto.style.display = "none";
+      btn.innerHTML = "Leia mais";
+    }
+  });
